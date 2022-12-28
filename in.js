0b7125f14e4c0bffac072596594e0d45ed38e4be
ahror9393 <ahrordjumabaev@gmail.com>
2022-12-28 19:49:22 +0500

    sasa

---
+++ in.js
@@ -0,0 +1,10 @@
+
+[2.Comparison operators, strict equality](https://www.jschallenger.com/javascript-practice/javascript-fundamentals/comparison-strict-equality)
+
+Solution
+
+```js
+function myFunction(a, b){
+return a === b}
+
+```
